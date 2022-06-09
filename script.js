079c3b82adc55b6e8ba85158a2c2f8e82e3f02b3
colinplancich <colin.plancich@gmail.com>
2022-06-08 20:05:22 -0700

    beginning pseudocoding for script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("JS file is linked.")
+
+// Rock beats Scissors
+// Paper beats Rock
+// Scissors beats Paper
+
+// Prompt user to make a choice of R, P, or S
+
+// Verify user has chosen one of these three options
+
+// Confirm user choice
+
+// Generate random choice for computer
+
+// Show game result
+
+// Show total wins / losses & ties
+
+// Ask user if they want to play again
